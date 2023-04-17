2dbcced1d221e9b745747b07a091c6d9183d709f
krudliere <krudliere@users.noreply.github.com>
2023-04-17 01:31:25 +0000

    Added jobitems template and api endpoint for JOBS

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Chicago, IL',
+    'salary': '150,000'
+  },
+    {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Chicago, IL',
+    'salary': '175,000'
+  },
+    {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': '150,000'
+  },
+    {
+    'id': 4,
+    'title': 'Fronted Engineer',
+    'location': 'Remote'
+  }
+]
+
 @app.route("/")
 def helloWorld():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name="Tutorian")
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 print(__name__)
 if __name__ == "__main__":
