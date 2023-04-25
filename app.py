716cbed536a604d3261745463acf9dfe5309bbdf
krudliere <krudliere@users.noreply.github.com>
2023-04-25 01:30:02 +0000

    dynamic pages read from the database

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
 from database import load_jobs_from_db
+from database import load_job_from_db
 
 app = Flask(__name__)
 
@@ -15,6 +16,14 @@ def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if job is None:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', job=job)
 
 print(__name__)
 if __name__ == "__main__":
